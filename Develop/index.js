9693805e551d81fe9caabf9170ad23cfae96ff2c
Jaydon Goodrich <jaydongoodrich@gmail.com>
2020-09-27 14:59:23 -0600

    questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,17 +1,59 @@
 const inquirer = require('inquirer');
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?'
-        }
-    ])
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is your project's title?"
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Please provide installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide instructions on how to use your project:'
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Please provide contribution instructions:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide test instructions:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license is most suitable for your project?',
+        choices: ['GNU AGPLv3', 'GNU GRLv3', 'GNU LGPLv3', 'Mozilla Public', 'Apache', 'MIT', 'Boost Software', 'The Unlicense']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'instructions',
+        message: 'Instructions for contacting with additional questions:'
+    },
 ];
 
 // function to write README file
@@ -25,3 +67,7 @@ function init() {
 
 // function call to initialize program
 init();
+
+
+inquirer
+    .prompt(questions)
