bedfd7359ab7b23404707009c14cbb0034aab840
Jaydon Goodrich <jaydongoodrich@gmail.com>
2020-09-27 13:54:13 -0600

    initial files

---
+++ Develop/index.js
@@ -0,0 +1,27 @@
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
